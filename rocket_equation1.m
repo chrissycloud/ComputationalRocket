8a909b4877ad9e93b292454168340b59368b3b19
GaBe141 <gabe_devine@hotmail.com>
2019-10-10 11:59:30 +0100

    Add files via upload

--- rocket_equation1.m
+++ rocket_equation1.m
@@ -6,9 +6,17 @@
 % mf = final mass
 % deltaV = max change in velocity of vehicle
 % deltaV = Isp*g*ln(m0/mf) = ve*ln(m0/mf)
-ve = 2550;
-g = 9.81;
-m0 = 45702;
-mf = 4932;
-deltaV = ve*log(m0/mf)
+% stage 1
+clc
+clear all
 
+Isp1 = 263;
+g0 = 9.81;
+m0 = 2766913;
+m1 = m0-2290000;
+deltaV1 = Isp1*g0*log(m0/m1)
+
+% stage 2
+Isp2 = 421;
+m2 = m0-(496200+2290000);
+deltaV2 = Isp2*g0*log(m1/m2)
