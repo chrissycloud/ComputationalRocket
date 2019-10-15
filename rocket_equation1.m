904257d45b1df502ac29b40f3fdc2ee491dcc86b
GaBe141 <gabe_devine@hotmail.com>
2019-10-15 10:56:29 +0100

    Add files via upload

--- rocket_equation1.m
+++ rocket_equation1.m
@@ -12,11 +12,20 @@ clear all
 
 Isp1 = 263;
 g0 = 9.81;
-m0 = 2766913;
-m1 = m0-2290000;
+m0 = 2970000;
+m1 = m0-2290000
 deltaV1 = Isp1*g0*log(m0/m1)
 
 % stage 2
 Isp2 = 421;
 m2 = m0-(496200+2290000);
 deltaV2 = Isp2*g0*log(m1/m2)
+
+% stage 3
+
+Isp3 = 421;
+m3 = m2-(123000);
+deltaV3 = Isp3*g0*log(m2/m3)
+
+%total deltaV
+sumdeltaV = deltaV1+deltaV2+deltaV3
