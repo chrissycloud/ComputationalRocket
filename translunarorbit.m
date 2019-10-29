a9ae40d59eaf57352dfcd7bddea1e8fc3f7a5c8e
GaBe141 <gabe_devine@hotmail.com>
2019-10-29 13:37:36 +0000

    Add files via upload

---
+++ translunarorbit.m
@@ -0,0 +1,46 @@
+% v = velocity at current postition
+% mu = is the GM of the central body
+% r = the distance from the central body
+% totalE = v^2/2 + mu/r
+% mue = GM of the earth
+% mum = GM of the moon
+% re = low earth orbit altitude
+% rm = low moon orbit altitude
+% am = semi-major axis of the moon orbit around the earth
+% vleo = orbital velocity in a low earth orbit
+% vleo = sqrt(mue/re)
+% vp = orbital velocity at periapsis
+% vp = sqrt(2am*mue/re(am+re))
+% deltaVinject =  vp - vleo
+% vllo = orbital speed in a low lunar orbit
+% vllo = sqrt(mum/rm)
+% vm = sqrt(mue/am)
+% va = velocity in the transfer orbit at the radius of the moon
+% va = sqrt(2re*mue/am(am+re))
+% vl = velocity at low moon orbit
+% vl = sqrt(((vm-va)^2)+(2mum/rm))
+% deltaVsinsert = vl - vllo
+% totaldeltaV = vp - vleo + vl - vllo
+
+clc
+clear all
+
+mue = 3.986*10^14;
+mum = 4.905*10^12;
+re = 200;
+rm = 100;
+am = 4600;
+
+vleo = sqrt(mue/re);
+vp = sqrt((2.*am.*mue/(re.*(re+am))));
+
+deltaVinject = vp - vleo
+
+vllo = sqrt(mum/rm);
+vm = sqrt(mue/am);
+va = sqrt((2.*re.*mue)/(am.*(am+re)));
+vl = sqrt(((vm-va).^2)+(2.*mum/rm));
+
+deltaVsinsert = vl - vllo
+
+totaldeltaV = vp - vleo + vl - vllo
